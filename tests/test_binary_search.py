eae28089b8d67413f84d35681e2c56f57276573b
Bárbara Gomes <barbaragomes@dcc.ufmg.br>
2021-12-02 18:39:59 +0100

    fix: fixture name for binary search

--- tests/test_binary_search.py
+++ tests/test_binary_search.py
@@ -4,85 +4,85 @@ from datalg.binary_search import binary_search, recursive_binary_search
 
 
 @pytest.fixture
-def stack():
+def array():
     return []
 
 
-def test_search_middle_element(stack):
+def test_search_middle_element(array):
     for i in range(11):
-        stack.append(i)
-    assert binary_search(stack, 5) == 5
+        array.append(i)
+    assert binary_search(array, 5) == 5
 
 
-def test_search_element_in_left_half(stack):
+def test_search_element_in_left_half(array):
     for i in range(10):
-        stack.append(i)
-    assert binary_search(stack, 2) == 2
+        array.append(i)
+    assert binary_search(array, 2) == 2
 
 
-def test_search_element_in_right_half(stack):
+def test_search_element_in_right_half(array):
     for i in range(10):
-        stack.append(i)
-    assert binary_search(stack, 7) == 7
+        array.append(i)
+    assert binary_search(array, 7) == 7
 
 
-def test_search_first_element(stack):
+def test_search_first_element(array):
     for i in range(10):
-        stack.append(i)
-    assert binary_search(stack, 0) == 0
+        array.append(i)
+    assert binary_search(array, 0) == 0
 
 
-def test_search_last_element(stack):
+def test_search_last_element(array):
     for i in range(10):
-        stack.append(i)
-    assert binary_search(stack, 9) == 9
+        array.append(i)
+    assert binary_search(array, 9) == 9
 
 
-def test_search_nonexisting_element(stack):
+def test_search_nonexisting_element(array):
     for i in range(10):
-        stack.append(i)
-    assert binary_search(stack, 20) == -1
+        array.append(i)
+    assert binary_search(array, 20) == -1
 
 
-def test_search_in_empty_stack(stack):
-    assert binary_search(stack, 20) == -1
+def test_search_in_empty_array(array):
+    assert binary_search(array, 20) == -1
 
 
-def test_recursive_search_middle_element(stack):
+def test_recursive_search_middle_element(array):
     for i in range(11):
-        stack.append(i)
-    assert recursive_binary_search(stack, 5) == 5
+        array.append(i)
+    assert recursive_binary_search(array, 5) == 5
 
 
-def test_recursive_search_element_in_left_half(stack):
+def test_recursive_search_element_in_left_half(array):
     for i in range(10):
-        stack.append(i)
-    assert recursive_binary_search(stack, 2) == 2
+        array.append(i)
+    assert recursive_binary_search(array, 2) == 2
 
 
-def test_recursive_search_element_in_right_half(stack):
+def test_recursive_search_element_in_right_half(array):
     for i in range(10):
-        stack.append(i)
-    assert recursive_binary_search(stack, 7) == 7
+        array.append(i)
+    assert recursive_binary_search(array, 7) == 7
 
 
-def test_recursive_search_first_element(stack):
+def test_recursive_search_first_element(array):
     for i in range(10):
-        stack.append(i)
-    assert recursive_binary_search(stack, 0) == 0
+        array.append(i)
+    assert recursive_binary_search(array, 0) == 0
 
 
-def test_recursive_search_last_element(stack):
+def test_recursive_search_last_element(array):
     for i in range(10):
-        stack.append(i)
-    assert recursive_binary_search(stack, 9) == 9
+        array.append(i)
+    assert recursive_binary_search(array, 9) == 9
 
 
-def test_recursive_search_nonexisting_element(stack):
+def test_recursive_search_nonexisting_element(array):
     for i in range(10):
-        stack.append(i)
-    assert recursive_binary_search(stack, 20) == -1
+        array.append(i)
+    assert recursive_binary_search(array, 20) == -1
 
 
-def test_recursive_search_in_empty_stack(stack):
-    assert recursive_binary_search(stack, 20) == -1
+def test_recursive_search_in_empty_array(array):
+    assert recursive_binary_search(array, 20) == -1
